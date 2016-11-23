8b09a78addd8f14a31cb3e78d80ac4e03ce9ef1d
shaocc <kugou2008.happy@163.com>
2016-11-23 16:37:43 +0800

    updata

--- stream/service/cache.go
+++ stream/service/cache.go
@@ -4,6 +4,7 @@ type Cache struct {
 	As   *Accounts    // 用户列表
 	Sas  *StreamAddrs // stream地址缓存列表
 	Cids *conIDs      // 用户链接缓存，映射account
+	bts  *btCache     // 广播主题缓存,保存用户的acc
 }
 
 func NewCache() *Cache {
@@ -11,6 +12,7 @@ func NewCache() *Cache {
 		As:   NewAccounts(),
 		Sas:  NewStreamAddrs(),
 		Cids: newconIDs(),
+		bts:  newbtCache(),
 	}
 	return cache
 }
